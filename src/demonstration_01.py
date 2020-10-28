bedaaf0753c6d1a696b69be090cccf838063ab7a
JenVest2020 <jenvest2019@gmail.com>
2020-10-27 21:02:55 -0500

    demo1 completed

--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -2,12 +2,28 @@
 Challenge #1:
 
 Create a function that takes two numbers as arguments and return their sum.
+1 + 2
+label = value or expression
+a = 1
+b = 2
+c = a + b (is an expression) => evaluates to 3 => c = 3
+
+return keyword
+return c
+
+
 
 Examples:
 - addition(3, 2) ➞ 5
 - addition(-3, -6) ➞ -9
 - addition(7, 3) ➞ 10
+set a sum to the value of the expression a plus b
+sum = a + b
+return our sum to the caller
+return sum
 """
 def addition(a, b):
-    # Your code here
+    print(addition(3, 2))
+    print (addition(-3, -6))
+    print(addition(7, 3))
 
